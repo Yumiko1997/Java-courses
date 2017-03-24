e9f5be438a87660eb67e78b22144bdfc2e309d0a
Dmytro <murin1997@gmail.com>
2017-03-24 16:53:16 +0200

    Math Standart

---
+++ MathStandart.java
@@ -0,0 +1,18 @@
+
+public class MathStandart {
+        public static void main(String[] arg) {
+            int first = Integer.valueOf(arg[0]);
+            int second = Integer.valueOf(arg[1]);
+            double three = Double.valueOf(arg[2]);
+            double four = Double.valueOf(arg[2]);
+            int sum = first + second;
+            int minus = first - second;
+            double multiply = three * four;
+            double Divide = three / four;
+            System.out.println("Sum " +sum);
+            System.out.println("Minus " +minus);
+            System.out.println("Multiply " +multiply);
+            System.out.println("Del " +Divide);
+            System.out.println("Degree'" + Math.pow(first, second));
+        }
+}
